15ed3b16c2076549b91e5edad744d0b34f3eaeba
Ben Lisneuf <happytreefriend2004@gmail.com>
2019-06-30 15:39:41 +0100

    button drawer fixed

--- mdc/MDCDrawer.svelte
+++ mdc/MDCDrawer.svelte
@@ -50,7 +50,9 @@
 <header class="mdc-top-app-bar app-bar" id="app-bar">
     <div class="mdc-top-app-bar__row">
       <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-        <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">menu</a>
+        <button class="mdc-button">
+          <i class="material-icons mdc-top-app-bar__navigation-icon">menu</i>
+        </button>
         <span class="mdc-top-app-bar__title">{title}</span>
       </section>
     </div>
